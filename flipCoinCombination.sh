010e5a759aa31ac7117410f2b58acf03d799dec9
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-20 17:11:05 +0530

    Modified dictionary name and added code for doublet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-declare -A coin
-h=0
-t=0
 read -p "How many times you want to flip:" num
 
-function flip()
+function singletFlip()
 {
 	flip=$((RANDOM%2))
 
@@ -13,27 +10,86 @@ function flip()
 	then
 		echo "Head-$flip"
 		((h++))
-		coin[h]=$h
+		singlet[h]=$h
 	else
 		echo "Tail-$flip"
 		((t++))
-		coin[t]=$t
+		singlet[t]=$t
 	fi
 }
 
+function doubletFlip()
+{
+	flip=$((RANDOM%4))
+	if [[ $flip == 0 ]]
+	then
+		((hh++))
+		doublet[hh]=$hh
+	elif [[ $flip == 1 ]]
+	then
+		((tt++))
+		doublet[tt]=$tt
+	elif [[ $flip == 2 ]]
+	then
+		((ht++))
+		doublet[ht]=$ht
+	else
+		((th++))
+		doublet[th]=$th
+	fi
+}
+
+
+declare -A singlet
+
+h=0
+t=0
+
 for (( i=0;i<$num;i++ ))
 do
-	flip
+	singletFlip
 done
 
-for i in ${!coin[@]}
+for i in ${!singlet[@]}
 do
 
-	echo "$i - ${coin[$i]}"
+	echo "$i - ${singlet[$i]}"
 	if [[ $i -eq $h ]]
 	then
-		echo "Singlet Head Percentage H : `echo "scale=1; ${coin[$i]}*100/$num" | bc ` %"
+		echo "Singlet Head Percentage H : `echo "scale=1; ${singlet[$i]}*100/$num" | bc ` %"
 	else
-		echo "Singlet Head Percentage H : `echo "scale=1; ${coin[$i]}*100/$num" | bc ` %"
+		echo "Singlet Tail Percentage T : `echo "scale=1; ${singlet[$i]}*100/$num" | bc ` %"
 	fi
+done
+
+
+declare -A doublet
+
+
+hh=0
+tt=0
+ht=0
+th=0
+
+
+for (( i=0;i<$num;i++))
+do
+	doubletFlip
+done
+
+for j in ${!doublet[@]}
+do
+    echo "$j - ${doublet[$j]}"
+    if [[ $j -eq hh ]]
+    then
+        echo "Doublet Head-Head Percentage HH : `echo "scale=1; ${doublet[$j]}*100/$num" | bc ` %"
+    elif [[ $j -eq tt ]]
+    then
+        echo "Doublet Tail-Tail Percentage TT : `echo "scale=1; ${doublet[$j]}*100/$num" | bc ` %"
+    elif [[ $j -eq ht ]]
+    then
+        echo "Doublet Head-Tail Percentage HT : `echo "scale=1; ${doublet[$j]}*100/$num" | bc ` %"
+    else
+        echo "Doublet Tail-Head Percentage TH : `echo "scale=1; ${doublet[$j]}*100/$num" | bc ` %"
+    fi
 done
