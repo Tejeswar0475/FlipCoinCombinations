4b0f76f6133c87555707c8a00d42010f5f6ab0c8
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-20 16:25:16 +0530

    Added code for multiple time fliping to get percent

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
-flip=$((RANDOM%2))
 
-if [ $flip == 1 ]
-then
-	echo "Head-$flip"
-else
-	echo "Tail-$flip"
-fi
+declare -A coin
+h=0
+t=0
+read -p "How many times you want to flip:" num
+
+function flip()
+{
+	flip=$((RANDOM%2))
+
+	if [ $flip == 1 ]
+	then
+		echo "Head-$flip"
+		((h++))
+		coin[h]=$h
+	else
+		echo "Tail-$flip"
+		((t++))
+		coin[t]=$t
+	fi
+}
+
+for (( i=0;i<$num;i++ ))
+do
+	flip
+done
+
+for i in ${!coin[@]}
+do
+
+	echo "$i - ${coin[$i]}"
+	if [[ $i -eq $h ]]
+	then
+		echo "Singlet Head Percentage H : `echo "scale=1; ${coin[$i]}*100/$num" | bc ` %"
+	else
+		echo "Singlet Head Percentage H : `echo "scale=1; ${coin[$i]}*100/$num" | bc ` %"
+	fi
+done
