6772afee2bc277c8688f8cd14d304eae06cc0cca
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-20 17:45:16 +0530

    Added code for triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -8,11 +8,10 @@ function singletFlip()
 
 	if [ $flip == 1 ]
 	then
-		echo "Head-$flip"
 		((h++))
 		singlet[h]=$h
 	else
-		echo "Tail-$flip"
+
 		((t++))
 		singlet[t]=$t
 	fi
@@ -38,6 +37,45 @@ function doubletFlip()
 		doublet[th]=$th
 	fi
 }
+
+function tripletFlip()
+{
+	flip=$((RANDOM%8))
+	if [ $flip -eq 0 ]
+	then
+		((hhh++))
+		triplet[hhh]=$hhh
+	elif [ $flip -eq 1 ]
+	then
+		((hht++))
+		triplet[hht]=$hht
+	elif [ $flip -eq 2 ]
+	then
+		((hth++))
+		triplet[hth]=$hth
+	elif [ $flip -eq 3 ]
+	then
+		((htt++))
+		triplet[htt]=$htt
+	elif [ $flip -eq 4 ]
+	then
+		((thh++))
+		triplet[thh]=$thh
+	elif [ $flip -eq 5 ]
+	then
+		((tht++))
+		triplet[tht]=$tht
+	elif [ $flip -eq 6 ]
+	then
+		((tth++))
+		triplet[tth]=$tth
+	else
+		((ttt++))
+		triplet[ttt]=$ttt
+	fi
+}
+
+
 
 
 declare -A singlet
@@ -93,3 +131,53 @@ do
         echo "Doublet Tail-Head Percentage TH : `echo "scale=1; ${doublet[$j]}*100/$num" | bc ` %"
     fi
 done
+
+
+declare -A triplet
+
+hhh=0
+hht=0
+hth=0
+htt=0
+
+ttt=0
+thh=0
+tht=0
+tth=0
+
+for (( i=0; i<$num; i++ ))
+do
+	tripletFlip
+done
+
+for k in ${!triplet[@]}
+do
+    echo "$k - ${triplet[$k]}"
+    if [[ $k -eq hhh ]]
+    then
+        echo "Triplet H-H-H percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq hht ]]
+    then
+        echo "Triplet H-H-T percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq hth ]]
+    then
+        echo "Triplet H-T-H percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq htt ]]
+    then
+        echo "Triplet H-T-T percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq thh ]]
+    then
+        echo "Triplet T-H-H percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq tht ]]
+    then
+        echo "Triplet T-H-T percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    elif [[ $k -eq tth ]]
+    then
+        echo "Triplet T-T-H percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    else
+        echo "Triplet T-T-T percentage : `echo "scale=1; ${triplet[$k]}*100/$num" | bc ` %"
+    fi
+done
+
+
+
